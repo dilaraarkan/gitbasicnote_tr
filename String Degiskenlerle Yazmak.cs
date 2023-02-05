746f41fca09ab1664c8bbe81aab5aa8aba417c01
Dilara Arkan <arkandilara@gmail.com>
2023-02-05 17:48:37 +0300

    Create String Degiskenlerle Yazmak.cs

---
+++ String Degiskenlerle Yazmak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsol_Dilara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string birinci = "Dilara";
+            string ikinci = "Unity Certified Instructor " + birinci;
+            string ucuncu = "Unity Certified Instructor " + ikinci + " " + "Arkan"
+
+            Console.WriteLine(ucuncu);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
