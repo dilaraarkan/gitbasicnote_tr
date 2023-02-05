4332a9878776a4658d11dd0f71c7061fca53162e
Dilara Arkan <arkandilara@gmail.com>
2023-02-05 16:53:30 +0300

    Sayısal Operasyonlar Eklendi

---
+++ Sayısal Operasyon 4 İşlem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplama = 35+34;
+            int cikarma = 35-34;
+            int carpim = 35*34;
+            int bolme = 35/34;
+
+            Console.WriteLine("Toplam " + toplama + "\nCikarma " + cikarma + "\n Carpim " + carpim + "\nBolme " + bolme);
+
+            Console.ReadLine();
+        }
+    }
+}
