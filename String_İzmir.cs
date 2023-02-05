e111ca36fde1af5cf490ce669441c607f302690f
Dilara Arkan <arkandilara@gmail.com>
2023-02-05 17:53:15 +0300

    Create String_İzmir.cs

---
+++ String_İzmir.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_App
+{
+	class Program
+	{
+        static void Main(string[] args)
+		{
+			// İzmir'de bomba yedim ve tanesi 5 liraydı ve hava sıcaklığı 25,5 dereceydi
+			string sehirIsmi = "Izmir";
+            Console.Write(sehirIsmi);
+			
+
